51d4edf0aabac7417ad1bd68f70eda5d6b7af7c7
Your Name <vonam235689741.work@gmail.com>
2022-01-26 23:37:30 +0700

    update Home Contact

--- VoTaNam-HomeContact/cache/58015dffed2f2bed574e5cbf49d479498e454af6.php
+++ VoTaNam-HomeContact/cache/58015dffed2f2bed574e5cbf49d479498e454af6.php
@@ -1,22 +1,23 @@
+<link href="https://fonts.googleapis.com/css2?family=Open+Sans:wght@400;500;600;700;800&display=swap" rel="stylesheet">
 <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css">
 <link rel="stylesheet" href="/main.css">
 <div class="c437d4e3f8eef461baf72527652334c98" id="c476e7b540034451f9c97dfdf180490ca">
     <!-- Write your code here -->
-    <!-- <h1>{{ $greetings }}</h1> -->
-    <!-- <div>{{$content->title}}</div> -->
-    <!-- <img src="{{$content->thumbnail}}" alt="">
+    <!-- <h1><?php echo e($greetings); ?></h1> -->
+    <!-- <div><?php echo e($content->title); ?></div> -->
+    <!-- <img src="<?php echo e($content->thumbnail); ?>" alt="">
     <div class="container">
-        <p>{{$content->title}}</p>
+        <p><?php echo e($content->title); ?></p>
     </div> -->
 
     <div class="contact">
         
-            <img src="{{$content->thumbnail}}" alt="">
+            <img src="<?php echo e($content->thumbnail); ?>" alt="">
         
         
         <div class="container">
             <div class="box-container">
-                <p>{{$content->title}}</p>
+                <p><?php echo e($content->title); ?></p>
                 <span>Hãy cho chúng tối biết!</span>
                 <a href="#">
                     <i class="fas fa-caret-right"></i>LIÊN HỆ NGAY
@@ -30,3 +31,4 @@
     <!-- Write your code here -->
 </div>
 <script src="/main.js"></script>
+<?php /**PATH D:\ANFLASH\anflash-2-components\VoTaNam-HomeContact\src\HomeContact/index.blade.php ENDPATH**/ ?>
--- VoTaNam-HomeContact/src/HomeContact/index.blade.php
+++ VoTaNam-HomeContact/src/HomeContact/index.blade.php
@@ -1,9 +1,33 @@
+<link href="https://fonts.googleapis.com/css2?family=Open+Sans:wght@400;500;600;700;800&display=swap" rel="stylesheet">
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css">
 <link rel="stylesheet" href="/main.css">
 <div class="c437d4e3f8eef461baf72527652334c98" id="c476e7b540034451f9c97dfdf180490ca">
     <!-- Write your code here -->
-    <h1>{{ $greetings }}</h1>
-    <div>{{$content->title}}</div>
-    <img src="{{$content->thumbnail}}" alt="">
+    <!-- <h1>{{ $greetings }}</h1> -->
+    <!-- <div>{{$content->title}}</div> -->
+    <!-- <img src="{{$content->thumbnail}}" alt="">
+    <div class="container">
+        <p>{{$content->title}}</p>
+    </div> -->
+
+    <div class="contact">
+        
+            <img src="{{$content->thumbnail}}" alt="">
+        
+        
+        <div class="container">
+            <div class="box-container">
+                <p>{{$content->title}}</p>
+                <span>Hãy cho chúng tối biết!</span>
+                <a href="#">
+                    <i class="fas fa-caret-right"></i>LIÊN HỆ NGAY
+                </a>
+            </div>
+        </div>
+    </div>
+        
+
+    
     <!-- Write your code here -->
 </div>
 <script src="/main.js"></script>
